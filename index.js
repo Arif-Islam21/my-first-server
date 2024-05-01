044c45332aadb60b0625d6c7d5f62eb9921b8c80
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-01 15:09:15 +0600

    can get data from the server

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const phones = require("./phones.json");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Arifs phone info comming soon toon");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("Data for id", id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
 });
 
 app.listen(port, () => {
